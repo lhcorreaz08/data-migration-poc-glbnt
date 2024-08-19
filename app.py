ef1db61733b74b5a9127d107925ee034d0904da8
Luis Correa <luis_correa.tekton@skyairline.com>
2024-08-19 10:26:29 -0500

    config flask basic file and dockerfiles

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from google.cloud import storage
+from datetime import datetime
+from collections import defaultdict, Counter
+import json
+from typing import List, Tuple, Optional
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+# Configurar el cliente de Google Cloud Storage
+storage_client = storage.Client()
+
+# Get the environment variables
+app.config['DEBUG'] = os.environ.get('FLASK_DEBUG')
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
